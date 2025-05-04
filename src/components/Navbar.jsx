0f9d54112cfa2d9a304efd9f5cc8d94a4207f124
Owitt Juada <owittj@Owitts-MacBook-Air.local>
2025-05-04 14:38:04 +0800

    third  commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 19, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
         <div className="max-w-5xl mx-auto px-4">
             <div className="flex justify-between items-center h-16">
-                <a href="#home" className="font-mono text-xl font-bold text-white"> logo </a>
+                <a href="#home" className="font-mono text-xl font-bold text-white"> Owitt's Portfolio </a>
 
                 <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
                     &#9776;
